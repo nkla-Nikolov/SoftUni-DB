085fef0e7de2e9ed09034aeb635ec2919e1605f4
nkla-Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2022-05-05 23:44:47 +0300

    Initial commit

---
+++ Subqueries and Joins/AddressesWithTowns.sql
@@ -0,0 +1,4 @@
+SELECT TOP(50) FirstName, LastName, t.Name AS [Town], a.AddressText FROM Employees AS e
+	JOIN Addresses AS a ON e.AddressID = a.AddressID
+	JOIN Towns AS t ON a.TownID = t.TownID
+	ORDER BY FirstName, LastName
---
+++ Subqueries and Joins/EmployeesHiredAfter.sql
@@ -0,0 +1,5 @@
+SELECT FirstName, LastName, HireDate, d.Name AS [DeptName] FROM Employees AS e
+	JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+	WHERE e.HireDate > '1999/01/01' AND
+	(d.Name = 'Sales' OR d.Name = 'Finance')
+	ORDER BY HireDate
