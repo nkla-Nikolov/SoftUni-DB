320cdf519aef2d2e59caf66a27c5449b696f2edc
nkla-Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2022-05-06 00:20:15 +0300

    Initial commit

---
+++ Subqueries and Joins/Employee24.sql
@@ -0,0 +1,9 @@
+SELECT e.EmployeeID, FirstName,
+	CASE
+		WHEN YEAR(p.StartDate) >= 2005 THEN NULL
+		ELSE p.Name
+	END AS ProjectName
+FROM Employees AS e
+	JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
+	JOIN Projects AS p ON ep.ProjectID = p.ProjectID
+WHERE e.EmployeeID = 24
