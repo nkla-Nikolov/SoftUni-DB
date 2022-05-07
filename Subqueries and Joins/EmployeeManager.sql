2659dfa8a396f7c284f9136a913dc228e613d423
nkla-Nikolov <90271515+nkla-Nikolov@users.noreply.github.com>
2022-05-07 17:04:37 +0300

    Added last problems

---
+++ Subqueries and Joins/EmployeeManager.sql
@@ -0,0 +1,4 @@
+SELECT e.EmployeeID, e.FirstName, e.ManagerID, m.FirstName AS [ManagerName] FROM Employees AS e
+	JOIN Employees as M ON e.ManagerID = m.EmployeeID
+	WHERE e.ManagerID = 3 OR e.ManagerID = 7
+	ORDER BY e.EmployeeID
